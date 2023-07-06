0dbc9b2daba2d0c7bc293071454acc5c340a4077
Fredrik Ronquist <fredrik.ronquist@nrm.se>
2023-07-06 14:58:52 +0200

    Corrected the clustal30_ag_fasta_format contents. Previously was halfgap now is allgaps.

--- data/clustal30_ag_fasta_format/gbfilter.sh
+++ data/clustal30_ag_fasta_format/gbfilter.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/bin/bash -l
+
+#SBATCH -A snic2017-7-283
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 10:00
+#SBATCH -J Gblocks
+#SBATCH -o allgaps.log
 
 echo $(date)
 
-for f in $(ls *.faa)
+for f in $(ls ../../aligned_buscos/fasta_format/*.faa)
 do
-	../../Gblocks_0.91b/Gblocks $f -p=t -t=p -b5=h -e=-ag
+	/proj/snic2017-7-283/aln_synergus/Gblocks_0.91b/Gblocks $f -p=t -t=p -b5=a -e=-ag
 done
 
 wait
 echo $(date)
-
